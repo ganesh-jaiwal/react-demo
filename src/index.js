46d60060c874bd55b8d10041d3e5081784af387a
Ganesh Jaiwal <ganeshjaiwal05@gmail.com>
2018-09-18 23:03:37 +0530

    refactore code to class

--- src/index.js
+++ src/index.js
@@ -1,19 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyD0sGGH3AXnb75C4RoDj3vIxK1vEuc_WVs';
 
 // Create new component.
 
-const App = () => {
-	return (
-		<div>
-			<SearchBar />
-		</div>
-	);
+class App extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = { videos: [] };
+
+		YTSearch({key: API_KEY, term: 'MostlySane'}, ( videos ) => {
+			this.setState({ videos });
+		});
+	}
+	render () {
+		return (
+			<div>
+				<SearchBar />
+			</div>
+		);
+	}
 }
 
 // Render abow generated component into the DOM.
- ReactDOM.render(<App />, document.querySelector('.container'));
+ReactDOM.render(<App />, document.querySelector('.container'));
