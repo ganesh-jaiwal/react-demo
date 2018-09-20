7b4b1dbabc87f1b6506c892693bd76d5b6050dad
Ganesh Jaiwal <ganeshjaiwal05@gmail.com>
2018-09-20 23:37:10 +0530

    search video list code is added

--- src/index.js
+++ src/index.js
@@ -1,30 +1,41 @@
-import React, { Component } from 'react';
+import React, {
+    Component
+} from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
 const API_KEY = 'AIzaSyD0sGGH3AXnb75C4RoDj3vIxK1vEuc_WVs';
 
 // Create new component.
 
 class App extends Component {
-	constructor(props) {
-		super(props);
+    constructor( props ) {
+        super( props );
 
-		this.state = { videos: [] };
+        this.state = {
+            videos: []
+        };
 
-		YTSearch({key: API_KEY, term: 'MostlySane'}, ( videos ) => {
-			this.setState({ videos });
-		});
-	}
-	render () {
-		return (
-			<div>
+        YTSearch( {
+            key: API_KEY,
+            term: 'MostlySane'
+        }, ( videos ) => {
+            this.setState( {
+                videos
+            } );
+        } );
+    }
+    render() {
+        return (
+            <div>
 				<SearchBar />
+				<VideoList videos = {this.state.videos} />
 			</div>
-		);
-	}
+        );
+    }
 }
 
 // Render abow generated component into the DOM.
-ReactDOM.render(<App />, document.querySelector('.container'));
+ReactDOM.render( <App />, document.querySelector( '.container' ) );
